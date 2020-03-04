73e4e2d1707b0794214dfb25699c1c56fe1183b4
Christian Berendt <berendt@betacloud-solutions.de>
2020-03-04 12:46:10 +0100

    Cleanup the cleanup scripts

--- scripts/cleanup-docker.sh
+++ scripts/cleanup-docker.sh
@@ -2,34 +2,18 @@
 
 # Available environment variables
 #
-# BUILD_ID
-# DOCKER_NAMESPACE
 # OPENSTACK_VERSION
-# OSISM_VERSION
 
 # Set default values
 
-BUILD_ID=${BUILD_ID:-$(date +%Y%m%d)}
-DOCKER_NAMESPACE=${DOCKER_NAMESPACE:-osism}
 OPENSTACK_VERSION=${OPENSTACK_VERSION:-master}
-OSISM_VERSION=${OSISM_VERSION:-latest}
-
-KOLLA_TYPE=ubuntu-source
-SOURCE_DOCKER_TAG=build-$BUILD_ID
 
 HASH_DOCKER_KOLLA_DOCKER=$(git rev-parse --short HEAD)
 HASH_RELEASE=$(cd release; git rev-parse --short HEAD)
 
-if [[ $(git name-rev --name-only HEAD) == "master" ]]; then
-    OSISM_VERSION=latest
-else
-    tag=$(git describe --exact-match HEAD)
-    OSISM_VERSION=${tag:1}
-fi
-
 docker system prune \
   --all \
   --force \
-  --filter "label=io.osism.docker_kolla_docker=$HASH_DOCKER_KOLLA_DOCKER" \
-  --filter "label=io.osism.openstack=$OPENSTACK_VERSION" \
-  --filter "label=io.osism.release=$HASH_RELEASE"
+  --filter "label=de.osism.commit.docker_kolla_docker=$HASH_DOCKER_KOLLA_DOCKER" \
+  --filter "label=de.osism.commit.release=$HASH_RELEASE" \
+  --filter "label=de.osism.release.openstack=$OPENSTACK_VERSION"
--- scripts/cleanup-repository.sh
+++ scripts/cleanup-repository.sh
@@ -1,20 +1,4 @@
 #!/usr/bin/env bash
 
-# Available environment variables
-#
-# OPENSTACK_VERSION
-
-# Set default values
-
-OPENSTACK_VERSION=${OPENSTACK_VERSION:-master}
-
-rm -f images.txt*
-rm -f kolla-build-*.log
-rm -f kolla-build.conf
-rm -f overlays/$OPENSTACK_VERSION/base/apt_preferences.ubuntu
-rm -f apt_preferences.ubuntu
-rm -f templates/$OPENSTACK_VERSION/template-overrides.j2
-rm -f template-overrides.j2
-rm -f versions.txt*
-
-rm -rf release tarballs kolla
+git clean -n
+git clean -n -d
