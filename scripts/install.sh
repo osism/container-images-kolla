2a96e17ab68c59da033fbe13ba9e4bf751df3fb0
Christian Berendt <berendt@betacloud-solutions.de>
2020-04-15 15:20:08 +0200

    Add missing packages to the install.sh script

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,13 @@ fi
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get -y install docker-ce parallel python3-pip
+sudo apt-get -y install \
+    docker-ce \
+    parallel \
+    python3-dev \
+    python3-pip \
+    python3-setuptools \
+    python3-wheel
 echo '{ "experimental": true }' | sudo tee /etc/docker/daemon.json
 sudo service docker restart
 
