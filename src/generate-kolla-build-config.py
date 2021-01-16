0e26016b3a259cb875c39d7ebf49594d9dfb0ec9
Christian Berendt <berendt@betacloud-solutions.de>
2021-01-16 21:44:30 +0100

    Improve the generate-kolla-build-config.py script
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- src/generate-kolla-build-config.py
+++ src/generate-kolla-build-config.py
@@ -103,6 +103,14 @@ loader = jinja2.FileSystemLoader(searchpath="templates/%s" % OPENSTACK_VERSION)
 environment = jinja2.Environment(loader=loader)
 template = environment.get_template(TEMPLATE_FILE)
 
+# NOTE: Starting with Victoria, only patched tarballs are included in the
+#       kolla-build.conf file.
+
+if versions['openstack_version'] in ["victoria", "wallaby", "master"]:
+    patched_projects = os.listdir("patches/%s/" %
+                                  versions['openstack_version'])
+    projects = [x for x in projects if x['name'] in patched_projects]
+
 template_data = {
     "base": KOLLA_BASE,
     "base_tag": KOLLA_BASE_TAG,
