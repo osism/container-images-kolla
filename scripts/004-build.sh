b87e96a107aee968d290f9d9214636a97dcfa7bb
Christian Berendt <berendt@osism.tech>
2023-07-02 16:03:50 +0200

    Squash base images (#325)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/004-build.sh
+++ scripts/004-build.sh
@@ -6,19 +6,15 @@ set -x
 #
 # BUILD_ID
 # BUILD_OPTS
-# BUILD_TYPE
 # KOLLA_IMAGES
 # OPENSTACK_VERSION
-# SQUASH
 # VERSION
 
 # Set default values
 
 BUILD_ID=${BUILD_ID:-$(date +%Y%m%d)}
-BUILD_TYPE=${BUILD_TYPE:-all}
 OPENSTACK_VERSION=${OPENSTACK_VERSION:-latest}
 VERSION=${VERSION:-latest}
-SQUASH=${SQUASH:-false}
 
 KOLLA_CONF=kolla-build.conf
 
@@ -33,38 +29,35 @@ fi
 export VERSION
 export OPENSTACK_VERSION
 
-if [[ $SQUASH == "true" ]]; then
-    BUILD_OPTS+=" --squash"
-fi
-
 if [[ -z "$KOLLA_IMAGES" ]]; then
     KOLLA_IMAGES="$(python3 src/get-projects-from-versions-file.py)"
 fi
 
+KOLLA_IMAGES_BASE=""
+for baseimage in $(find kolla/docker -name '*-base' | sort | xargs -n1 basename | awk -F - '{print $1}'); do
+    if [[ $KOLLA_IMAGES == *"$baseimage"* ]]; then
+        KOLLA_IMAGES_BASE+=" ${baseimage}-base"
+    fi
+done
+
+# Build & squash base images
+
+kolla-build \
+  --template-override templates/$OPENSTACK_VERSION/template-overrides.j2 \
+  --config-file $KOLLA_CONF \
+  --pull \
+  --squash \
+  $BUILD_OPTS \
+  $KOLLA_IMAGES_BASE 2>&1 | tee kolla-build-$BUILD_ID.log
+
 # Build images
 
-if [[ $BUILD_TYPE == "base" ]]; then
-    KOLLA_IMAGES_BASE=""
-    for baseimage in $(find kolla/docker -name '*-base' | sort | xargs -n1 basename | awk -F - '{print $1}'); do
-        if [[ $KOLLA_IMAGES == *"$baseimage"* ]]; then
-            KOLLA_IMAGES_BASE+=" ${baseimage}-base"
-        fi
-    done
-
-    kolla-build \
-      --template-override templates/$OPENSTACK_VERSION/template-overrides.j2 \
-      --config-file $KOLLA_CONF \
-      --pull \
-      $BUILD_OPTS \
-      $KOLLA_IMAGES_BASE 2>&1 | tee kolla-build-$BUILD_ID.log
-else
-    kolla-build \
-      --template-override templates/$OPENSTACK_VERSION/template-overrides.j2 \
-      --config-file $KOLLA_CONF \
-      --skip-existing \
-      $BUILD_OPTS \
-      $KOLLA_IMAGES 2>&1 | tee kolla-build-$BUILD_ID.log
-fi
+kolla-build \
+  --template-override templates/$OPENSTACK_VERSION/template-overrides.j2 \
+  --config-file $KOLLA_CONF \
+  --skip-existing \
+  $BUILD_OPTS \
+  $KOLLA_IMAGES 2>&1 | tee kolla-build-$BUILD_ID.log
 
 if grep -q "Failed with status: error" kolla-build-$BUILD_ID.log; then
     echo "ERROR: Not all the required images could be built."
