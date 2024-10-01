05831044c54d3b9cf918a93099e39192810bb300
Christian Berendt <berendt@osism.tech>
2024-10-01 21:46:04 +0200

    Fix handling of new --platform parameter (#489)
    Related to 5ff178777560509f897b9f379db6a55963a0b428
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/004-build.sh
+++ scripts/004-build.sh
@@ -22,13 +22,6 @@ VERSION=${VERSION:-latest}
 
 KOLLA_CONF=kolla-build.conf
 
-if [[ $BASE_ARCH == "x86_64" ]]; then
-    PLATFORM="linux/amd64"
-elif [[ $BASE_ARCH == "aarch64" ]]; then
-    PLATFORM="linux/arm64"
-    docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-fi
-
 # NOTE: For builds for a specific release, the OpenStack version is taken from the release repository.
 if [[ $VERSION != "latest" ]]; then
     OPENSTACK_VERSION=$(grep "openstack_version:" release/$VERSION/openstack.yml | awk -F': ' '{ print $2 }')
@@ -43,8 +36,15 @@ export OPENSTACK_VERSION
 # For ARM64 we currently only support the images that are required on the compute plane.
 if [[ "$BASE_ARCH" == "aarch64" ]]; then
     KOLLA_IMAGES="^fluentd ^cron ^nova-libvirt ^nova-ssh ^nova-compute ^neutron-metadata-agent ^ceilometer-compute ^ovn-controller ^openvswitch-vswitchd ^openvswitch-db-server"
+    PLATFORM="linux/arm64"
+    docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 elif [[ -z "$KOLLA_IMAGES" ]]; then
     KOLLA_IMAGES="$(python3 src/get-projects-from-versions-file.py)"
+    PLATFORM="linux/amd64"
+fi
+
+if [[ "$OPENSTACK_VERSION" == "2024.1" ]]; then
+    $PLATFORM_OPTS="--platform $PLATFORM"
 fi
 
 # Build images
@@ -58,22 +58,22 @@ if [[ $BUILD_TYPE == "base" ]]; then
     done
 
     kolla-build \
-      --platform $PLATFORM \
       --base-arch $BASE_ARCH \
       --debug \
       --template-override templates/$OPENSTACK_VERSION/template-overrides.j2 \
       --config-file $KOLLA_CONF \
       --pull \
+      $PLATFORM_OPTS \
       $BUILD_OPTS \
       $KOLLA_IMAGES_BASE 2>&1 | tee kolla-build-$BUILD_ID.log
 else
     kolla-build \
-      --platform $PLATFORM \
       --base-arch $BASE_ARCH \
       --debug \
       --template-override templates/$OPENSTACK_VERSION/template-overrides.j2 \
       --config-file $KOLLA_CONF \
       --skip-existing \
+      $PLATFORM_OPTS \
       $BUILD_OPTS \
       $KOLLA_IMAGES 2>&1 | tee kolla-build-$BUILD_ID.log
 fi
