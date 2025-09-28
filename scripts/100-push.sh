04073cdcc7da7ffc2af4a7b1092b2496ac939b61
Christian Berendt <berendt@osism.tech>
2025-09-28 13:06:21 +0200

    Do not sign base images with cosign
    We do not push them. It's not necessary to sign them.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/100-push.sh
+++ scripts/100-push.sh
@@ -46,6 +46,6 @@ cat other.log
 curl -O -L "https://github.com/sigstore/cosign/releases/latest/download/cosign-linux-amd64"
 chmod +x cosign-linux-amd64
 
-for image in $(cat $LSTFILE); do
+for image in $(cat $LSTFILE | grep -v base); do
     ./cosign-linux-amd64 sign --yes --key env://COSIGN_PRIVATE_KEY "$image"
 done
