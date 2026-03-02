d7fda77f81c909c4d8a355c35ed11b675165fbbf
Christian Berendt <berendt@osism.tech>
2026-03-02 12:06:20 +0100

    2025.2: build all images required by metalbox
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- defaults/2025.2.sh
+++ defaults/2025.2.sh
@@ -1,4 +1,4 @@
 export BUILD_ID=$(date +%Y%m%d)
-export KOLLA_IMAGES="^ironic"
+export KOLLA_IMAGES="^ironic ^redis ^rabbitmq ^mariadb ^common ^keystone ^memcached"
 export KOLLA_TYPE=
 export BASE_VERSION=24.04
