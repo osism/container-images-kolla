3a35b17b4a38a2cb4a26ba09fad4213ddefd937a
Christian Berendt <berendt@osism.tech>
2026-03-04 10:28:02 +0100

    2025.2: add missing images required by metalbox
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- defaults/2025.2.sh
+++ defaults/2025.2.sh
@@ -1,4 +1,4 @@
 export BUILD_ID=$(date +%Y%m%d)
-export KOLLA_IMAGES="^ironic ^redis ^rabbitmq ^mariadb ^common ^keystone ^memcached"
+export KOLLA_IMAGES="^ironic ^redis ^rabbitmq ^mariadb ^keystone ^memcached ^cron ^kolla-toolbox ^fluentd"
 export KOLLA_TYPE=
 export BASE_VERSION=24.04
