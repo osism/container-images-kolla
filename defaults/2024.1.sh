51cf4d3c10900d52ff2c5f799ab8f56ee99e45a1
Dr. Jens Harbott <harbott@osism.tech>
2026-08-11 20:23:34 +0200

    designate: Add backported CVE fixes
    Signed-off-by: Dr. Jens Harbott <harbott@osism.tech>

--- defaults/2024.1.sh
+++ defaults/2024.1.sh
@@ -1,4 +1,4 @@
 export BUILD_ID=$(date +%Y%m%d)
-export KOLLA_IMAGES="^nova ^glance ^keystone ^neutron"
+export KOLLA_IMAGES="^nova ^glance ^keystone ^neutron ^designate"
 export KOLLA_TYPE=
 export BASE_VERSION=22.04
